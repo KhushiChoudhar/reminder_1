8a1be8c94a12c2caa66a0f8ce88fed41e094a22c
KhushiChoudhar <104776552+KhushiChoudhar@users.noreply.github.com>
2023-02-12 11:21:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import asyncio
 from flask import Flask,request
 from firebase_admin import credentials,db,firestore
 cred = firebase_admin.credentials.Certificate("telebot.json")
-firebase_admin.intialize_app(cred,name="this_App")
+firebase_admin.initialize_app(cred,name="this_App")
 Default_App = firebase_admin.initialize_app(cred, {
 	'databaseURL': "https://telebot-1ec79-default-rtdb.firebaseio.com"
 	})
